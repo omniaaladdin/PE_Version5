e501fe17cb2eda3fc2406662ae7715c36939b43d
omniaaladdin <133586748+omniaaladdin@users.noreply.github.com>
2023-12-16 01:02:56 -0800

    Update main.cpp
    multiply changed by main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 // Function to calculate the summation of two numbers
-double mult(double a, double b) {
+double multiply(double a, double b) {
     return a * b;
 }
 
@@ -18,7 +18,7 @@ int main() {
     std::cin >> num2;
 
     // Perform operations and print results
-    std::cout << "Operation: " << mult(num1, num2) << std::endl;
+    std::cout << "Operation: " << multiply(num1, num2) << std::endl;
    
     return 0;
 }
